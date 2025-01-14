7d52567977c0a51936c213cbb5b5a85205af216e
rickytabe <rickytabe2@gmail.com>
2025-01-14 07:20:54 +0100

    added git repo link to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div  className="animate-bounce">
-        <div className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 animate-gradient bg-[length:200%_200%]">
-          Welcome. TutorFinder is coming soon.
+        <div className="text-4xl font-bold text-center grid-flow-row text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 animate-gradient bg-[length:200%_200%]">
+          Welcome. TutorFinder is coming soon, come back later.
+        </div>
+        <div>
+          Take a look at our github repo
+          <a href="https://github.com/rickytabe/TutorFinderApp.git">See Repository</a>
         </div>
       </div>
     </div>
