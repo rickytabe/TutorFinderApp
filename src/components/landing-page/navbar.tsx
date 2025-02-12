62059a31e6a4251387c59f682200c5bb10d2acdc
rickytabe <rickytabe2@gmail.com>
2025-02-12 19:43:06 +0100

    fixed client site routing issues by adding vercel.json

--- src/components/landing-page/navbar.tsx
+++ src/components/landing-page/navbar.tsx
@@ -44,7 +44,7 @@ const NavBar: React.FC<NavBarProps> = ({ scrollToSection, activeSection }) => {
       isActive ? "w-full" : "w-0 group-hover:w-full"
     }`;
   const menuContainerStyles =
-    "hidden md:block backdrop-blur-md bg-white/10 p-4 border-gray-200 border rounded-lg";
+    "hidden lg:block backdrop-blur-md bg-white/10 p-4 border-gray-200 border rounded-lg";
   const buttonBaseStyles = "px-6 py-2 text-sm font-medium";
   const signupButtonStyles = `${buttonBaseStyles} text-white bg-blue-600 rounded hover:bg-blue-700`;
   const loginButtonStyles = `${buttonBaseStyles} text-blue-600 border border-blue-600 rounded hover:bg-blue-100 hover:border-blue-700`;
